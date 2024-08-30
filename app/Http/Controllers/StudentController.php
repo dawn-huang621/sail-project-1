14402c66ecdc6216d8cbb94293474bea61f59228
Dawn Huang <q2379685@gmail.com>
2024-08-30 20:40:17 +0800

    git init

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\BaseController;
+
+class Controller extends BaseController
+{
+    public function addSave(){
+        dd('dddd');
+    }
+}
